59ea68fefc9a61dc90c8e0473c5ab594b9e45f1c
FRA2721 <cimino.francesco36@gmail.com>
2022-10-29 16:31:16 +0200

    emails generator

--- js/script.js
+++ js/script.js
@@ -2,5 +2,34 @@
 // proj: Vue Email List;
 // lang: js;
 
-// debug
-alert("red");
+// init
+const {createApp} = Vue;
+
+createApp( {
+    data() {
+
+        return {
+            // array-for-emails-(empty)
+            emailsArray: [ ],    
+        }
+    },
+    
+    created() {
+
+        // for-loop
+        for (let i = 0; i < 10; i++) {
+
+            axios
+
+            // random-email-generator
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+                .then((resp)=> {
+                    this.rndEmail = resp.data.response
+                    this.emailsArray.push(this.rndEmail)
+
+                });
+        }
+   },
+    
+}).mount("#root");
